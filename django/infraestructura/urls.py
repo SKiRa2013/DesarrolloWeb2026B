e679931e68380fca448d3b76e72c9d8eb04f731d
SKiRa2013 <skira250128@gmail.com>
2026-09-01 12:26:34 -0500

    feat(base.html, detalle.html): Create detail page to see server specific details, refactor(index.html, infraestructura/urls.py, infraestructura/views.py): Change the template architecture so base.html renders all possible new subpages.

--- django/infraestructura/urls.py
+++ django/infraestructura/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from infraestructura.views import lista_servidores
+from infraestructura.views import lista_servidores, detalle_servidor
 
 urlpatterns = [
     path('', lista_servidores, name='home_servidores'),
+    path('servidor/<int:pkey>/', detalle_servidor, name='detalle_servidor'),
 ]
--- django/infraestructura/views.py
+++ django/infraestructura/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from infraestructura.models import NodoServidor
 
+def detalle_servidor(request, pkey):
+    servidor = get_object_or_404(NodoServidor, pk=pkey)
+    contexto = {'nodo': servidor}
+    return render(request, 'infraestructura/detalle.html', contexto)
+    
 def lista_servidores(request):
 	servidores = NodoServidor.objects.all()
 	contexto = {'servidores': servidores}
